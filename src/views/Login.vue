71099984298f21f7ca8e804a692fc1593ad00514
phpdvlpmt <phpdvlpmt@gmail.com>
2020-01-17 14:35:40 +0100

    login page

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="email" /><br />
+    <input type="password" placeholder="password" />
+    <button>Přihlásit</button>
+    <p>Nemáte účet? Můžete si ho vytvořit</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
+<style lang="scss" scoped></style>
